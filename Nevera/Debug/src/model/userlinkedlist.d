e48c100d808b593ac6da04c4df6a0feafb40a2b5
Jorge Calatayud Maeso <jorge.cmaeso@alumnos.upm.es>
2017-07-10 14:33:34 +0200

    Fixed commit error

--- Nevera/Debug/src/model/userlinkedlist.d
+++
@@ -1,6 +0,0 @@
-src/model/userlinkedlist.o: ../src/model/userlinkedlist.c \
- ../src/model/userlinkedlist.h ../src/model/usuarios.h
-
-../src/model/userlinkedlist.h:
-
-../src/model/usuarios.h:
--- Nevera/Debug/src/model/usuarios.d
+++
@@ -1,3 +0,0 @@
-src/model/usuarios.o: ../src/model/usuarios.c ../src/model/usuarios.h
-
-../src/model/usuarios.h:
